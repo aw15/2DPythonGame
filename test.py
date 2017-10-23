2ae6856aa3933ad9307dcca4f6ed4799661f76fa
aw15 <didxodbs0987@naver.com>
2017-10-24 01:59:23 +0900

    10-24 리소스 수집 및 테스트

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from pico2d import *
-import random
+from Object import *
 
 open_canvas()
 
 image = load_image('resource\grass.png')
 wall_image = load_image('resource\wall.png')
+charator_image = load_image('resource\charactor\MainGuy.png')
+
+Object:Object
 
 while(True):
     clear_canvas()
@@ -13,5 +16,10 @@ while(True):
             image.draw(50+i*100,50+j*100)
     for i in range(0,6):
         wall_image.draw(500,50+i*100)
+
+
+    charator_image.clip_draw(g_index*41,36,41,36,100,100)
+
+    delay(0.2)
     update_canvas()
 close_canvas()
