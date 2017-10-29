b21642d9ad199414eea40a92d993a9c379186a23
aw15 <didxodbs0987@naver.com>
2017-10-29 13:25:31 +0900

    unit resigter

--- Object.py
+++ Object.py
@@ -11,9 +11,14 @@ class Object:
         self.increase = []
         self.x = 0
         self.y = 0
+        self.maxIndex =0
+        self.horizental=True
     def SetPosition(self,x,y):
         self.x = x
         self.y = y
+    def SetMaxIndex(self, param,isHorizen=True):
+        self.maxIndex = param
+        self.horizental = isHorizen
     def SetDirection(self,up,down,right, left, increase):
         self.up = up
         self.down = down
@@ -21,13 +26,25 @@ class Object:
         self.right = right
         self.increase = increase
     def update(self, dir):
-        if (dir == 0):
-            self.image.clip_draw(self.animationIndex*self.up[0],self.up[1],self.increase[0],self.increase[1],self.x,self.y)
-        if (dir == 1):
-            self.image.clip_draw(self.animationIndex * self.down[0], self.down[1], self.increase[0], self.increase[1], self.x, self.y)
-        if (dir == 2):
-            self.image.clip_draw(self.animationIndex * self.right[0], self.down[1], self.increase[0], self.increase[1], self.x, self.y)
-        if (dir == 3):
-            self.image.clip_draw(self.animationIndex * self.left[0], self.left[1], self.increase[0], self.increase[1], self.x, self.y)
-        self.animationIndex+=1
-        self.animationIndex%=3
+        if(self.horizental):
+            if (dir == 0):
+                self.image.clip_draw(self.animationIndex*self.up[0],self.up[1],self.increase[0],self.increase[1],self.x,self.y)
+            if (dir == 1):
+                self.image.clip_draw(self.animationIndex * self.down[0], self.down[1], self.increase[0], self.increase[1], self.x, self.y)
+            if (dir == 2):
+                self.image.clip_draw(self.animationIndex * self.right[0], self.right[1], self.increase[0], self.increase[1], self.x, self.y)
+            if (dir == 3):
+                self.image.clip_draw(self.animationIndex * self.left[0], self.left[1], self.increase[0], self.increase[1], self.x, self.y)
+        else:
+            if (dir == 0):
+                self.image.clip_draw(self.up[0],self.animationIndex*self.up[1],self.increase[0],self.increase[1],self.x,self.y)
+            if (dir == 1):
+                self.image.clip_draw( self.down[0], self.animationIndex *self.down[1], self.increase[0], self.increase[1], self.x, self.y)
+            if (dir == 2):
+                self.image.clip_draw( self.right[0], self.animationIndex *self.right[1], self.increase[0], self.increase[1], self.x, self.y)
+            if (dir == 3):
+                self.image.clip_draw(self.left[0],self.animationIndex *  self.left[1], self.increase[0], self.increase[1], self.x, self.y)
+        self.animationIndex = self.animationIndex+ 1
+        #print(self.animationIndex)
+        if(self.animationIndex > self.maxIndex):
+            self.animationIndex = 0
--- test.py
+++ test.py
@@ -1,14 +1,40 @@
 from pico2d import *
-from Object import *
+import Object
 
 open_canvas()
 
 image = load_image('resource\grass.png')
 wall_image = load_image('resource\wall.png')
-charator_image = load_image('resource\charactor\MainGuy.png')
+charator_image = load_image('resource\character\MainGuy.png')
 
-Object:Object
+#boy
+boy1 = Object.Object('resource\character\MainGuy.png')
+boy1.SetDirection([41,36],[41,108],[41,0],[43,72],[38,36])
+boy1.SetPosition(100,100)
+boy1.SetMaxIndex(2)
+#2단계---------------------------------------------------
+boy2 = Object.Object('resource\character\george.png')
+boy2.SetDirection([96,48],[0,48],[144,48],[48,48],[48,48])
+boy2.SetPosition(100,100)
+boy2.SetMaxIndex(3,False)
+#Unit Initailize, SetDirection(up,down,right, left, increase)
+# Albert = Object.Object(r'resource\character\unit\Albert.png')
+# Albert.SetDirection([16,48],[16,16],[16,32],[16,0],[16,16])
+# Albert.SetPosition(100,100)
+# Albert.SetMaxIndex(1)
+#---------------------------------------------------------
+unitList = []
+for i in range(1,42):
+    filename = '\c' + str(i)+'.png'
+    newObject = Object.Object(r'resource\character\unit' + filename)
+    newObject.SetDirection([16,48],[16,16],[16,32],[16,0],[16,16])
+    newObject.SetPosition(100,100)
+    newObject.SetMaxIndex(1)
+    unitList.append(newObject)
 
+
+g_index=0
+#0 = UP 1 = DOWN 2=RIGHT 3= LEFT
 while(True):
     clear_canvas()
     for i in range(0,8):
@@ -16,10 +42,13 @@ while(True):
             image.draw(50+i*100,50+j*100)
     for i in range(0,6):
         wall_image.draw(500,50+i*100)
+    # Albert.SetPosition(100+g_index, 100)
+    unitList[35].update(2)
 
-
-    charator_image.clip_draw(g_index*41,36,41,36,100,100)
-
-    delay(0.2)
+    # charator_image.clip_draw(g_index*41,108,41,36,100,100)
+    g_index = g_index+3
+    # if g_index>2:
+    #     g_index=0
+    delay(0.08)
     update_canvas()
 close_canvas()
